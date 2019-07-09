c9485e7c228d5c476075dd576f5cdf9562dd699f
ipalakot <ipalakot@gmail.com>
2019-07-09 22:55:13 +0200

    Btn Add / Update

--- symp4_dew_w1/src/Controller/BlogController.php
+++ symp4_dew_w1/src/Controller/BlogController.php
@@ -81,7 +81,8 @@ class BlogController extends AbstractController
         } 
         
         	return $this-> render ('blog/create.html.twig', [
-        			'formArticle' => $form->createView()
+        			'formArticle' => $form->createView(),
+        			'editMode' => $article->getId() !== null
         	]);
         }
         

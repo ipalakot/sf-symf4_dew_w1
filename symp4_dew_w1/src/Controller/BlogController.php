6fb0737f0d4ec66d55bb93d4004b91307f0dc02a
ipalakot <ipalakot@gmail.com>
2019-07-12 17:00:59 +0200

    Database User

--- symp4_dew_w1/src/Controller/BlogController.php
+++ symp4_dew_w1/src/Controller/BlogController.php
@@ -25,10 +25,8 @@ class BlogController extends AbstractController
 */
     public function index()
     {
-    	$repo = $this->getDoctrine()->getRepository(Article::class);
-    	
-    	$articles = $repo->findAll();
-    	
+    	$repo = $this->getDoctrine()->getRepository(Article::class);    	
+    	$articles = $repo->findAll();    	
     	return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
     		'articles'=>$articles,
@@ -43,48 +41,35 @@ class BlogController extends AbstractController
      public function home()
         {
             return $this->render('blog/home.html.twig', [
-
             ]);
-        }
-
-        
+        }        
         
 /**
   * @Route("/blog/new", name="blog_create")
   * @Route("/blog/{id}/edit", name="blog_edit")
 */
-        public function form(Article $article = null, Request $request, ObjectManager $manager) {
-        	
-        	if (!$article) {
-        		
-        		$article = new Article();
-     
-        	}
-        	
-        	
+        public function form(Article $article = null, Request $request, ObjectManager $manager) {        	
+        	if (!$article) {        		
+        		$article = new Article();     
+        	}        	        	
 #        	$form = $this->createFormBuilder($article)
 #        	->add('title', TextType::class)
 #        	->add('content', TextareaType::class)
 #        	->add('image', TextType::class)
 #        	->getForm();
         
-        $form = $this->createForm(ArticleType::class, $article);
-        	
-        $form -> handleRequest($request);
-        
+        $form = $this->createForm(ArticleType::class, $article);        	
+        $form -> handleRequest($request);        
         if($form-> isSubmitted() && $form->isValid()) 
         {	
         	if (!$article->getId()) {
         		$article-> setCreatedAt(new \DateTime());
-        	}
-
-        	
+        	}        	
         	$manager->persist($article);
         	$manager->flush();
         	
         	return $this->redirectToRoute('blog_show', ['id'=>$article->getId()]);
-        } 
-        
+        }         
         	return $this-> render ('blog/create.html.twig', [
         			'formArticle' => $form->createView(),
         			'editMode' => $article->getId() !== null
@@ -96,13 +81,10 @@ class BlogController extends AbstractController
 /**
  * @Route("/blog/article/{id}", name="blog_show")
 */
-     public function show($id)
-     
+     public function show($id)     
         {
-        	$repo=$this->getDoctrine()->getRepository(Article::class);
-        	
-        	$article = $repo->find($id);
-        	
+        	$repo=$this->getDoctrine()->getRepository(Article::class);        	
+        	$article = $repo->find($id);        	
         	return $this->render('blog/show.html.twig', [
         		'article'=>$article
             ]);
--- symp4_dew_w1/src/Form/ArticleType.php
+++ symp4_dew_w1/src/Form/ArticleType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ArticleType extends AbstractType
 {
@@ -13,6 +15,14 @@ class ArticleType extends AbstractType
     {
         $builder
             ->add('title')
+            -> add('category', EntityType::class, [
+            
+            // looks for choices from this entity
+            		'class' => Category::class,
+            // uses the User.username property as the visible option string
+            		'choice_label' => 'title',
+            		
+            ])
             ->add('content')
             ->add('image')
             ->add('createdAt')

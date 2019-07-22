6bf9529597cef95aa6b5c2fd97a775a7893a2b8a
ipalakot <ipalakot@gmail.com>
2019-07-22 18:33:41 +0200

    Constraint / Assert des formulaires

--- symp4_dew_w1/src/Entity/User.php
+++ symp4_dew_w1/src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -28,9 +29,13 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="8", minMessage="Le mot de passe doit faire au minimun 8 caractères")
      */
     private $password;
     
+    /**
+     * @Assert\EqualTo(propertyPath="password", message="votre mot de passe est different")
+     */
     public $confirm_password;
 
     public function getId(): ?int
